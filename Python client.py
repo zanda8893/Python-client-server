27f6e40454870901f00923b5a7f6d375e46f6886
zanda8893 <49042887+zanda8893@users.noreply.github.com>
2019-12-18 10:57:55 +0000

    Added client and server

---
+++ Python client.py
@@ -0,0 +1,14 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Create socket and connect it to server
+def connect():
+    s.connect(('localhost',8019))
+connect()
+# client loop
+while True:
+    message = input("Your Message: ")
+    s.send( message.encode('utf-8') )
+    print("Awaiting the reply...")
+    reply = s.recv( 1024 ).decode( 'utf-8' )
+    print("Recieved ", str(reply))
+    
---
+++ Python server.py
@@ -0,0 +1,37 @@
+import socket
+from time import sleep
+s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connected=False
+def serverstart():
+    HOST = '10.170.2.18'
+    PORT = 8019
+    s.bind((HOST, PORT))
+    s.listen(5) # Number of connections
+
+def connect():
+    global client #global varables to be used in chat()
+    global address
+    # Accept connections
+    client, address = s.accept()
+    print("Connected to", address)
+    if client != None:
+        return True
+
+def chat():
+    # Receive data and decode using utf-8
+    data = client.recv( 1024 ).decode( 'utf-8' )
+    if data != None:
+        print("Recieved :", repr(data))
+
+        # Send data to client in utf-8
+        reply = input("Reply :")
+        reply=str(reply)
+        client.sendall( reply.encode('utf-8') ) # Make sure data gets there with sendall()
+
+serverstart()
+while connected==False:
+    connected=connect()
+if connected==True:
+    while True:
+        sleep(1)
+        chat()
