c59ea8109365ab57a5bd74c293ca54f6c47890b3
zanda8893 <49042887+zanda8893@users.noreply.github.com>
2019-12-19 14:36:22 +0000

    Put chat send and receive into different functions

--- Python-client.py
+++ Python-client.py
@@ -1,34 +1,40 @@
 import socket
+import threading
 class client():
     def __init__(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.count = 0
         # Create s and connect it to server
     def connect(self):
+        global chatting
         self.s.connect(('localhost',8019))
-        self.chatting=True # Now Chatting
+        chatting=True # Now Chatting
         print("Connected to server")
     def disconnect(self):
         self.s.send( "%disconnect%".encode('utf-8') ) # send disconnect message to client
         self.s.close() # close socket
+        chatting=False # no longer chatting
         print("Disconnect from server \nPress anykey to exit") # Tell user about discconnect
-    # client loop
-    def chat(self):
-        print("type Q to quit")
-        while self.chatting==True: #While chatting
-            message = input("Your Message: ")
-            if message =="Q":
-                self.disconnect() #disconnect
-                self.chatting=False #No longer chatting
+    def send(self):
+        if self.count ==0:
+            print("type Q to quit")
+            self.count+=1
+        message = input("Your Message: ")
+        if message =="Q":
+            self.disconnect() #disconnect
+        else:
+            self.s.send( message.encode('utf-8') ) # send message encode with utf-8
+    def receive(self):
+            reply = self.s.recv( 1024 ).decode( 'utf-8' ) # Receive message decode with utf-8
+            if reply =="%disconnect%": # If client disconnects
+                self.disconnect() # Disconnect
+                print("Server disconnected")
             else:
-                self.s.send( message.encode('utf-8') ) # send message encode with utf-8
-                print("Awaiting the reply...")
-                reply = self.s.recv( 1024 ).decode( 'utf-8' ) # Receive message decode with utf-8
-                if reply =="%disconnect%": # If client disconnects
-                    self.disconnect() # Disconnect
-                    print("Server disconnected")
-                    self.chatting=False # no longer chatting
-                else:
-                    print("Recieved ", str(reply)) # print recieved message
+                print("Recieved ", str(reply)) # print recieved message
 client = client()
+print(threading.active_count())
+
 client.connect()
-client.chat()
+while chatting==True:
+    client.send()
+    client.receive()
