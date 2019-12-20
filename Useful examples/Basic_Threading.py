e735f0338c3b5770d0b8ee30e027fbde9064ceb3
zanda8893 <49042887+zanda8893@users.noreply.github.com>
2019-12-20 19:53:30 +0000

    Added better example

--- Useful examples/Basic_Threading.py
+++ Useful examples/Basic_Threading.py
@@ -1,16 +1,24 @@
 from threading import Thread
-def thread():
+from time import sleep
+string="thread "
+def thread(string):
     while True:
-        print("Thread 1")
-def thread1():
+        sleep(1)
+        print(string+"0")
+def thread1(string):
     while True:
-        print("Thread 2")
+        sleep(1)
+        print(string+"1")
 
-t1 = Thread(target = thread)
-t2 = Thread(target = thread1)
+t1 = Thread(target = thread, args=(string,))
+t2 = Thread(target = thread1, args=(string,))
 t1.setDaemon(True)
 t2.setDaemon(True)
+print("Starting t1")
 t1.start()
+print("t1 started")
+print("Starting t2")
 t2.start()
+print("t2 started")
 while True:
     pass
