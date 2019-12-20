6ec8a619919a2886955a025491a502c337de96fd
zanda8893 <49042887+zanda8893@users.noreply.github.com>
2019-12-20 13:43:22 +0000

    Split send and receive into seperate functions

--- Python-server.py
+++ Python-server.py
@@ -1,9 +1,9 @@
 import socket
 from time import sleep
 s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connected=False
 class server():
     def __init__(self):
+        self.chatting = False
         self.start()
         self.connect()
         self.chat()
@@ -14,34 +14,34 @@ class server():
         s.listen(5) # Number of connections
         print("Server started")
     def connect(self):
-        global client #global varables to be used in chat()
-        global address
         # Accept connections
-        client, address = s.accept()
-        print("Connected to", address)
-        if client != None:
+        self.client, self.address = s.accept()
+        print("Connected to", self.address)
+        if self.client != None:
             self.chatting=True
     def disconnect(self):
-        client.sendall("%disconnect%".encode('utf-8'))
+        self.client.sendall("%disconnect%".encode('utf-8'))
         s.close()
         print("Disconnected")
         self.chatting=False
         self.__init__()
-    def chat(self):
+    def receive(self):
         print("type Q to quit")
         # Receive data and decode using utf-8
         while self.chatting == True:
-            data = client.recv( 1024 ).decode( 'utf-8' )
+            data = self.client.recv( 1024 ).decode( 'utf-8' )
             if data != None:
                 if data =="%disconnect%":
                     self.disconnect()
                 else:
                     print("Recieved :", repr(data))
-                    # Send data to client in utf-8
-                    reply = input("Reply :")
-                    reply=str(reply)
-                    if reply =="Q":
-                        self.disconnect()
-                    else:
-                        client.sendall( reply.encode('utf-8') ) # Make sure data gets there with sendall()
+    def send(self):
+        while self.chatting==True:
+            # Send data to client in utf-8
+            reply = input("Reply :")
+            reply=str(reply)
+            if reply =="Q":
+                self.disconnect()
+            else:
+                self.client.sendall( reply.encode('utf-8') ) # Make sure data gets there with sendall()
 server()
