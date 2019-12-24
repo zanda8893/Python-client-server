63e49d2dec649ced2970d2fca380761d5bfa0e34
Blitzkriegs03 <f.p.theil@gmail.com>
2019-12-24 16:11:26 +0000

    fixed the raw_input

--- Useful examples/domain_names.py
+++ Useful examples/domain_names.py
@@ -1,7 +1,7 @@
 import socket
 #generally you loop this program
 
-ip = raw_input("input ip or domain name ")# idk why it wont let me use input() so im using raw_input()
+ip = input("input ip or domain name ")# idk why it wont let me use input() so im using raw_input()
 #i think it is my ide being quirky and running the wrong version of python
 try:
     ip = socket.gethostbyname(ip)
